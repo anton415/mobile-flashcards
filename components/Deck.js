6089d6a0c59bc8af7076ee0b9e17478c948ec223
Anton Serdyuchenko <anton415@gmail.com>
2019-03-28 06:51:14 +0300

    Add basic view for Deck.js

--- components/Deck.js
+++ components/Deck.js
@@ -1,15 +1,34 @@
 import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import { Text, View, Button } from 'react-native'
 import { getDeck } from '../utils/api'
 
 class Deck extends Component {
+
+
   render() {
     const { navigation } = this.props
     const deck = navigation.getParam('deck')
 
+    addCard = () => {
+      console.log('add card');
+    }
+
+    startQuiz = () => {
+      console.log('start quiz');
+    }
+
     return (
       <View>
         <Text>{deck.title}</Text>
+        <Text>{deck.questions.length} cards</Text>
+        <Button
+          onPress={addCard}
+          title="Add Card"
+        />
+        <Button
+          onPress={startQuiz}
+          title="Start Quiz"
+        />
       </View>
     )
   }
