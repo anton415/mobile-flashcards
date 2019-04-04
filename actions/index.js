4201a374943dc088b8a6a6c7f8028f48691d3852
Anton Serdyuchenko <anton415@gmail.com>
2019-04-04 06:35:55 +0300

    Move all actions code in one file. Add more actions.

--- actions/index.js
+++ actions/index.js
@@ -1,1 +1,42 @@
-export * from './decks'
+export const RECEIVE_DECKS = "RECEIVE_DECKS"
+export const ADD_DECK = "ADD_DECK"
+export const STUDIED_TODAY = "STUDIED_TODAY"
+export const ADD_QUESTION = "ADD_QUESTION"
+export const REMOVE_DECK = "REMOVE_DECK"
+
+
+export function receiveDecks(decks) {
+  return {
+    type: RECEIVE_DECKS,
+    decks
+  }
+}
+
+export function addDeck(deck) {
+  return {
+    type: ADD_DECK,
+    deck
+  }
+}
+
+export function deleteDeck(deckId) {
+  return {
+    type: REMOVE_DECK,
+    deckId
+  }
+}
+
+export function addQuestion(question) {
+  return {
+    type: ADD_QUESTION,
+    question
+  }
+}
+
+
+export function setStudiedToday(studiedToday) {
+  return {
+    type: STUDIED_TODAY,
+    studiedToday
+  }
+}
