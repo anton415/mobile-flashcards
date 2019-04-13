f317e2c5f267442a142f5474420125676dff3562
Anton Serdyuchenko <anton415@gmail.com>
2019-04-13 08:29:11 +0300

    Edit decks view.

--- components/Decks.js
+++ components/Decks.js
@@ -1,50 +1,88 @@
-import React, { Component } from 'react';
-import { View, Text, StyleSheet, ScrollView, Platform, TouchableOpacity } from 'react-native';
-import { getDecks } from '../utils/api';
-import Deck from './Deck';
-import { createStackNavigator } from 'react-navigation';
+import React, { Component } from 'react'
+import { View, ScrollView, Text, StyleSheet, Platform, TouchableOpacity, Animated } from 'react-native'
+import { connect } from 'react-redux'
+import { receiveDecks, addDeck, setStudied } from '../actions'
+import { getDailyReminderValue } from '../utils/helpers'
+import { fetchDecks, fetchStudied } from '../utils/api'
+import { white, purple, gray, lightPurp } from '../utils/colors'
+import HeaderView from './HeaderView'
+import DeckViewDetails from './DeckViewDetails'
+import { AppLoading } from 'expo'
 
 class Decks extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      decks: {}
-    }
+  state = {
+    ready: false,
+    opacity: new Animated.Value(0)
   }
 
   componentDidMount() {
-    getDecks().then(results => {
-      this.setState(() => ({ decks: results }))
-    })
+    const { dispatch } = this.props;
+    fetchDecks()
+      .then(decks => dispatch(receiveDecks(JSON.parse(decks))))
+      .then(() => fetchStudied())
+      .then(studied => dispatch(setStudied(JSON.parse(studied))))
+      .then(studied => {
+        if (!studied) {
+          return getDailyReminderValue();
+        }
+      })
+
+      .then(() => this.setState({ ready: true }));
   }
 
   render() {
-    const { decks } = this.state;
+    const { studied, decks } = this.props;
+    const { ready } = this.state;
+
+    if (ready === false) {
+      return <AppLoading />
+    }
 
     return (
       <ScrollView>
+        <HeaderView headerText={"Decks"} />
         <View>
-          <Text>Mobile flashcards</Text>
-          {Object.keys(decks).map(deck => {
-            return (
-              <View key={decks[deck].title}>
+          {decks && Object.keys(decks).length !== 0 ? (
+            Object.keys(decks).map(key => {
+              return decks[key] !== null ? (
                 <TouchableOpacity
-                  onPress={() => this.props.navigation.navigate('Deck', {
-                    deck: decks[deck]
-                  })}
-                 >
-                  <View>
-                    <Text>{decks[deck].title}</Text>
-                    <Text>{decks[deck].questions.length} cards</Text>
-                  </View>
+                  key={key}
+                  onPress={() => {
+                    Animated.timing(this.state.opacity, {
+                      toValue: 1,
+                      duration: 1000
+                    })
+                    return this.props.navigation.navigate("Deck", {
+                      deckId: key
+                    })
+                  }}
+                >
+                  <DeckViewDetails
+                    deckId={key}
+                    questionsLength={decks[key].questions.length}
+                  />
                 </TouchableOpacity>
-              </View>
-            )
-          })}
+              ) : null;
+            })
+          ) : (
+            <Text>Decks</Text>
+          )}
         </View>
       </ScrollView>
     )
   }
 }
 
-export default Decks
+const styles = StyleSheet.create({
+})
+
+function mapStateToProps(state) {
+  const { decks, studied } = state
+
+  return {
+    decks,
+    studied
+  }
+}
+
+export default connect(mapStateToProps)(Decks)
