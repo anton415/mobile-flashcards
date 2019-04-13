b97734afd2da8ebf1f6876b0fee3e270b96359d9
Anton Serdyuchenko <anton415@gmail.com>
2019-04-13 08:27:06 +0300

    Edit navigation.

--- navigation/MainNavigator.js
+++ navigation/MainNavigator.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { createAppContainer, createBottomTabNavigator, createStackNavigator } from 'react-navigation'
 import { FontAwesome, Ionicons } from '@expo/vector-icons'
+import { purple, white } from '../utils/colors'
 import Decks from '../components/Decks'
 import Deck from '../components/Deck'
 import AddCard from '../components/AddCard'
@@ -9,38 +10,79 @@ import Quiz from '../components/Quiz'
 import { Platform } from 'react-native'
 
 const Tabs = createBottomTabNavigator ({
-  DeckList: {
-    screen: Decks,
-    navigationOptions: {
-      tabBarLabel: 'Decks',
+    Decks: {
+      screen: Decks,
+      navigationOptions: {
+        tabBarLabel: 'Decks',
         tabBarIcon: ({ tintColor }) => (
           <Ionicons name="ios-bookmarks" size={30} color={tintColor} />
         )
+      }
+    },
+    NewDeck: {
+      screen: NewDeck,
+      navigationOptions: {
+        tabBarLabel: "NewDeck",
+        tabBarIcon: ({ tintColor }) => (
+          <FontAwesome name="plus-square" size={30} color={tintColor} />
+        )
+      }
     }
   },
-  NewDeck: {
-    screen: NewDeck,
+  {
     navigationOptions: {
-      tabBarLabel: 'NewDeck'
+      header: null
+    }
+  },
+  {
+    tabBarOptions: {
+      activeTintColor: Platform.OS === "ios" ? purple : white,
+      style: {
+        height: 56,
+        backgroundColor: Platform.OS === "ios" ? white : purple,
+        shadowColor: "rgba(0, 0, 0, 0.24)",
+        shadowOffset: {
+          width: 0,
+          height: 3
+        },
+        shadowRadius: 6,
+        shadowOpacity: 1
+      }
     }
   }
-})
+)
 
-const StackNavigator = createStackNavigator ({
+const MainNavigator = createStackNavigator ({
   Home: {
     screen: Tabs
   },
   Deck: {
-    screen: Deck
+    screen: Deck,
+    navigationOptions: {
+      headerTintColor: white,
+      headerStyle: {
+        backgroundColor: purple
+      }
+    }
   },
   AddCard: {
-    screen: AddCard
+    screen: AddCard,
+    navigationOptions: {
+      headerTintColor: white,
+      headerStyle: {
+        backgroundColor: purple
+      }
+    }
   },
   Quiz: {
-    screen: Quiz
+    screen: Quiz,
+    navigationOptions: {
+      headerTintColor: white,
+      headerStyle: {
+        backgroundColor: purple
+      }
+    }
   }
 })
-
-const MainNavigator = createAppContainer(StackNavigator)
 
 export default MainNavigator
