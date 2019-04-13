636d97a9a71653f3dffd221ed8af7df181643722
Anton Serdyuchenko <anton415@gmail.com>
2019-04-13 09:31:55 +0300

    New deck view.

--- components/NewDeck.js
+++ components/NewDeck.js
@@ -1,57 +1,95 @@
-import React, { Component } from 'react';
-import {
-  View,
-  KeyboardAvoidingView,
-  Text,
-  StyleSheet,
-  Platform,
-  TouchableOpacity,
-  TextInput,
-  Button
-} from 'react-native';
-import { saveDeckTitle } from '../utils/api';
+import React, { Component } from 'react'
+import { View, KeyboardAvoidingView, TouchableOpacity, Text, StyleSheet, TextInput } from 'react-native'
+import { getMetricMetaInfo, timeToString, getDailyReminderValue, clearLocalNotification,
+  setLocalNotification } from '../utils/helpers'
+import { Ionicons } from '@expo/vector-icons'
+import { submitDeck } from '../utils/api'
+import { connect } from 'react-redux'
+import { addDeck } from '../actions'
+import HeaderView from './HeaderView'
+import { white, gray } from '../utils/colors'
+import { NavigationActions } from 'react-navigation'
+import  ActionButton from './ActionButton'
+
 
 class NewDeck extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      inputText: ''
-    }
-  }
+  state = {
+    title: "",
+    questions: []
+  };
 
-  createNewDeck = () => {
-    return addNewDeckTitle(this.state.inputText)
-  }
+  submit = () => {
+    const deck = this.state;
+    const title = this.state.title;
 
+    if(!title) {
+    return alert("Please add the deck name")
+   }
 
+    this.props.dispatch(
+      addDeck({
+        [title]: deck
+      })
+    );
 
+    this.setState(() => ({ title: "" }));
+
+    this.toDeck(title);
+
+    submitDeck(title, deck);
+  };
+
+  toDeck = (key) => {
+    return this.props.navigation.navigate("Deck", {
+      deckId: key
+    })
+  };
   render() {
-    const { inputText } = this.state
-
-    submit = () => {
-      console.log('submit');
-      console.log('input: ', this.state.inputText);
-    }
-
     return (
-      <KeyboardAvoidingView behavior="padding">
-        <Text>
-          What is the title of your new deck?
-        </Text>
-        <View>
+      <KeyboardAvoidingView  style={styles.container} behavior="padding" enabled>
+        <HeaderView headerText={"New Deck"} />
+        <View style={styles.stretch}>
+          <Text style={styles.question}>
+            What is the title of your new deck?
+          </Text>
           <TextInput
-            onChangeText={inputText => this.setState({ inputText })}
-            value={inputText}
-            placeholder={'Deck Title'}
-          />
-          <Button
-            onPress={submit}
-            title="Submit"
+            style={styles.txtInput}
+            placeholder = "Deck Title"
+            placeholderTextColor = {gray}
+            onChangeText={title => this.setState({ title })}
+            value={this.state.title}
           />
         </View>
-      </KeyboardAvoidingView>
-    )
+        <ActionButton label={"CREATE DECK"} action={this.submit} />
+      </KeyboardAvoidingView >
+    );
   }
 }
 
-export default NewDeck
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    backgroundColor: white
+  },
+    stretch: {
+    flex: 1,
+    alignItems: 'stretch',
+    marginLeft: 30,
+    marginRight: 30,
+    padding: 30
+  },
+  question: {
+    fontSize: 25,
+    textAlign: "center"
+  },
+  txtInput: {
+    margin: 5,
+    padding: 5,
+    borderWidth: 2,
+    fontSize: 20,
+    borderRadius: 5
+  }
+})
+
+export default connect()(NewDeck)
